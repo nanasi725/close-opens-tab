2ac9a14d4c2c009a972741bdb077b1899bc171a0
nanasi725 <s1f102501787@iniad.org>
2026-06-22 17:15:30 +0900

    Open two tabs when a tab is closed

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,19 @@
+const NUMBER_OF_TABS_TO_OPEN = 2;
+
 chrome.tabs.onRemoved.addListener(async (_tabId, removeInfo) => {
   if (removeInfo.isWindowClosing) {
     return;
   }
 
   try {
-    await chrome.tabs.create({
-      windowId: removeInfo.windowId,
-      active: true
-    });
+    await Promise.all(
+      Array.from({ length: NUMBER_OF_TABS_TO_OPEN }, () =>
+        chrome.tabs.create({
+          windowId: removeInfo.windowId,
+          active: true
+        })
+      )
+    );
   } catch (error) {
     console.warn("新しいタブを開けなかったよ:", error);
   }
